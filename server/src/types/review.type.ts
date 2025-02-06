34f3fc9f55ac003386ba7dc18e557ffe7831fcce
RaRoiz <tataphon32@gmail.com>
2025-02-07 02:35:22 +0700

    bru

--- server/src/types/review.type.ts
+++ server/src/types/review.type.ts
@@ -1,4 +1,4 @@
-import { Static, t } from "elysia"
+import Elysia, { Static, t } from "elysia"
 
 export const _restaurantReview = t.Object({
     id: t.String(),
@@ -8,5 +8,17 @@ export const _restaurantReview = t.Object({
     created_at: t.Optional(t.Date()),
     updated_at: t.Optional(t.Date()),
 })
+export const _review = t.Object({
+    ..._restaurantReview.properties,
+})
 
-export type restaurantReview = Static<typeof _restaurantReview>
+export const restaurantReviewDto = new Elysia().model({
+    review: _review
+})
+
+export const _createReview = t.Object(t.Omit(_restaurantReview, ['id', 'created_at', 'updated_at']).properties)
+export const _updateReview = t.Object(t.Partial(_restaurantReview).properties)
+
+export type createReview = Static<typeof _createReview>
+export type updateReview = Static<typeof _updateReview>
+export type review = Static<typeof _review>
